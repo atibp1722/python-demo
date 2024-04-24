2906dc72fed24fa5e84d8eab3bbe9ae34a55511e
Atib Pokharel <atibp1722@gmail.com>
2024-04-24 16:10:38 +0545

    modified to display meaning that doesn't require clikcing on the button

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -12,17 +12,17 @@ class Dictionary:
         jp.Div(a=div,text='Start using the Dictionary App!',classes='text-5xl text-center m-2')
         jp.Div(a=div,text='Use the app to find meaning of English words.',classes='text-lg py-2 px-3')
 
-        input=jp.Div(a=div,classes='grid grid-cols-2')
-        answer=jp.Input(a=input,placeholder='Your word goes here...'
+        inputs=jp.Div(a=div,classes='grid grid-cols-2')
+        outputs=jp.Div(a=div,classes='m-2 p-2 text-lg border-2 h-40')
+
+        answer=jp.Input(a=inputs,placeholder='Your word goes here...',outputdiv=outputs
                  ,classes='m-2 bg-gray-150 border-2 border-2-green-200 rounded w-64 focus:bg-white focus:outline-none focus:border-red-350 py-2 px-3')
         
-        output=jp.Div(a=div,classes='m-2 p-2 text-lg border-2 h-40')
-        jp.Button(a=input,text='Get Definition',classes='border-2 text-black-400',
-                  click=cls.get_definition,outputdiv=output,inputbox=answer)
+        answer.on('input',cls.get_definition)
 
-        return wp
+        return wp 
     
     @staticmethod
     def get_definition(widget,msg):
-        meaning=dct.WordDefinition(widget.answer.value).get()
-        widget.output.text=" ".join(meaning)
+        meaning=dct.WordDefinition(widget.value).get()
+        widget.outputs.text=" ".join(meaning)
