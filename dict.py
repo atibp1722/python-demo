01177ccbb701fcee195851977eba60376c4136ee
Atib Pokharel <atibp1722@gmail.com>
2024-04-24 14:02:52 +0545

    created additional files for the app and test program by importing and running csv data using pandas

---
+++ dict.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class WordDefinition:
+
+    def __init__(self,word):
+        self.word=word
+    
+    def get_definition(self):
+        data=pd.read_csv('web-dict\words.csv')
+        return tuple(data.loc[data['word']==self.word]['definition'])
+    
+wrd=WordDefinition(word='data')
+print(wrd.get_definition())
