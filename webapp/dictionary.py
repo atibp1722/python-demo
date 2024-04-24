9866eeffc97b1f2362ebc00c67979a5de0b8260f
Atib Pokharel <atibp1722@gmail.com>
2024-04-24 16:00:35 +0545

    added button to get meaning for user entered word and display it

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -1,18 +1,28 @@
 import justpy as jp
+import dicts as dct
 
 class Dictionary:
 
     path='/dict'
 
-    def serve(self):
+    @classmethod
+    def serve(cls,req):
         wp=jp.QuasarPage(tailwind=True)
         div=jp.Div(a=wp,classes='bg-grey-200 h-screen')
         jp.Div(a=div,text='Start using the Dictionary App!',classes='text-5xl text-center m-2')
-        
         jp.Div(a=div,text='Use the app to find meaning of English words.',classes='text-lg py-2 px-3')
-        jp.Input(a=div,placeholder='Your word goes here...'
+
+        input=jp.Div(a=div,classes='grid grid-cols-2')
+        answer=jp.Input(a=input,placeholder='Your word goes here...'
                  ,classes='m-2 bg-gray-150 border-2 border-2-green-200 rounded w-64 focus:bg-white focus:outline-none focus:border-red-350 py-2 px-3')
         
-        jp.Div(a=div,classes='m-2 p-2 text-lg border-2 h-40')
+        output=jp.Div(a=div,classes='m-2 p-2 text-lg border-2 h-40')
+        jp.Button(a=input,text='Get Definition',classes='border-2 text-black-400',
+                  click=cls.get_definition,outputdiv=output,inputbox=answer)
 
         return wp
+    
+    @staticmethod
+    def get_definition(widget,msg):
+        meaning=dct.WordDefinition(widget.answer.value).get()
+        widget.output.text=" ".join(meaning)
