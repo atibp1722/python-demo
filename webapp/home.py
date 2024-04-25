80d03c909f019c3d8f72671b4f9e819a05048eae
Atib Pokharel <atibp1722@gmail.com>
2024-04-25 14:08:39 +0545

    created navigation menu for the home page

--- webapp/home.py
+++ webapp/home.py
@@ -13,12 +13,18 @@ class Home:
         toolbar=jp.QToolbar(a=header)
         
         drawer=jp.QDrawer(a=layout,show_if_above=True,v_model='left',bordered=True)
+        scroller=jp.QScrollArea(a=drawer,classes='fit')
+        q_list=jp.QList(a=scroller,classes='my-2')
+        jp.A(a=q_list,text='Dictionary',href='/dict',classes='p-2 m-2 text-2xl text-black-500 hover:text-red-500')
+        jp.Br(a=q_list)
+        jp.A(a=q_list,text='Home',href='/',classes='p-2 m-2 text-2xl text-black-500 hover:text-red-500')
+        jp.Br(a=q_list)
         jp.QBtn(a=toolbar,dense=True,round=True,flat=True,icon='menu',click=cls.move_drawer,drawer=drawer)
         jp.QToolbarTitle(a=toolbar,text='Welcome to Dictionary App')
 
         container=jp.QPageContainer(a=layout)
 
-        div=jp.Div(a=container,classes='bg-grey-200 h-screen')
+        div=jp.Div(a=container,classes='bg-grey-200 h-screen p-2')
         jp.Div(a=div,text='Welcome to Dictionary App',classes='text-5xl m-2 text-center')
         jp.Div(a=div,text='''It is a long established fact that a reader will be distracted by the readable content
                of a page when looking at its layout. The point of using Lorem Ipsum is that 
